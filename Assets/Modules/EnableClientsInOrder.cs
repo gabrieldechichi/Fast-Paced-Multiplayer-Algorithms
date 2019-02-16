2734b1730485afae60ad83030a975cc6b73dad04
root <root@DESKTOP-KKQOET0.localdomain>
2019-02-16 22:02:10 +0000

    enable (connect) clients in order + assign initial positions to clients

---
+++ Assets/Modules/EnableClientsInOrder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableClientsInOrder : MonoBehaviour {
+
+    [SerializeField] Client[] clients;
+
+    private IEnumerator Start()
+    {
+        foreach (var c in clients)
+        {
+            c.gameObject.SetActive(true);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
--- Assets/Modules/WorldSpace.cs
+++ Assets/Modules/WorldSpace.cs
@@ -5,14 +5,22 @@ public class WorldSpace : MonoBehaviour {
     [SerializeField] Entity localEntityPrefab;
     [SerializeField] Entity remoteEntityPrefab;
     [SerializeField] Client client;
+    [SerializeField] Transform[] initialPositions;
+
+    int currentPositionIndex;
 
 	public IEntity InstantiateEntity(string id, bool isLocal)
+    {
+        return InstantiateEntityInternal(id, isLocal);
+    }
+
+    Entity InstantiateEntityInternal(string id, bool isLocal)
     {
         var entityPrefab = isLocal ? localEntityPrefab : remoteEntityPrefab;
         var entity = Instantiate(entityPrefab).GetComponent<Entity>();
         entity.Setup(id, client, ChooseColor(id));
         entity.transform.SetParent(transform, false);
-        entity.transform.localPosition = Vector2.zero;
+        entity.transform.position = GetNewEntityPosition();
         return entity;
     }
 
@@ -20,5 +28,16 @@ public class WorldSpace : MonoBehaviour {
     {
         var intId = int.Parse(id);
         return intId % 2 == 0 ? Color.blue : Color.green;
+    }
+
+    Vector3 GetNewEntityPosition()
+    {
+        if (initialPositions.Length == 0)
+        {
+            return transform.TransformPoint(Vector3.zero);
+        }
+        var pos = initialPositions[currentPositionIndex % initialPositions.Length].position;
+        currentPositionIndex++;
+        return pos;
     }
 }
