1594544cadf709732fc630142d3ea5dcc883fed0
root <root@DESKTOP-KKQOET0.localdomain>
2019-02-17 09:39:48 +0000

    support moving entities separately

--- Assets/Modules/EntityController.cs
+++ Assets/Modules/EntityController.cs
@@ -6,15 +6,25 @@ using UnityEngine;
 public class EntityController : MonoBehaviour {
 
     [SerializeField] float speed = 5;
+    [SerializeField] bool useAWSD = false;
 
     Entity entity;
     Entity Entity { get { return entity ?? (entity = GetComponent<Entity>()); } }
 
     private void Update()
     {
-        Vector2 input = new Vector2(
-            Input.GetAxisRaw("Horizontal"), 0);
+        Entity.Move(GetInput()*speed*Time.deltaTime);
+    }
 
-        Entity.Move(input*speed*Time.deltaTime);
+    Vector2 GetInput()
+    {
+        return useAWSD ? GetInputWithKeys(KeyCode.A, KeyCode.D) : GetInputWithKeys(KeyCode.LeftArrow, KeyCode.RightArrow);
+    }
+
+    Vector2 GetInputWithKeys(KeyCode leftKey, KeyCode rightKey)
+    {
+        float input = Input.GetKey(leftKey) ? -1 : 0;
+        input += (Input.GetKey(rightKey)) ? 1 : 0;
+        return new Vector2(input, 0);
     }
 }
